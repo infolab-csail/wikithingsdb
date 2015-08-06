6bf9c9157eb4b3ddfaeb9dabb2b320bc9b4e9153
Michael Silver <yomichael88@yahoo.com>
2015-08-06 14:35:47 -0400

    partial setup of DB

--- wikithingsdb/models.py
+++ wikithingsdb/models.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python2
 
+from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Table, Column, Integer, String, Text, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import Table, Text
 
 Base = declarative_base()
+
+engine = create_engine(
+    'mysql://root:@localhost/py_wikipedia', pool_recycle=3600)
 
 
 class Type(Base):
     __tablename__ = "types"
 
     id = Column(Integer, primary_key=True)
-    type = Column(String)
+    type = Column(String(50))
 
     def __init__(self, type):
         self.type = type
@@ -23,7 +28,7 @@ class WikiClass(Base):
     __tablename__ = "classes"
 
     id = Column(Integer, primary_key=True)
-    class_name = Column(String)
+    class_name = Column(String(50))
 
     def __init__(self, class_name):
         self.class_name = class_name
@@ -36,7 +41,7 @@ class DbpediaClass(Base):
     __tablename__ = "dbpedia_classes"
 
     id = Column(Integer, primary_key=True)
-    dpedia_class = Column(String)
+    dpedia_class = Column(String(50))
 
     def __init__(self, dpedia_class):
         self.dpedia_class = dpedia_class
@@ -45,11 +50,11 @@ class DbpediaClass(Base):
         return '<DbpediaClass: %s>' % self.dpedia_class
 
 
-article_classes = Table('article_classes', Base.metadata,
-                        Column(
-                            'a_id', Integer, ForeignKey('page.page_id')),
-                        Column('c_id', Integer, ForeignKey('classes.id'))
-                        )
+# article_classes = Table('article_classes', Base.metadata,
+#                         Column(
+#                             'a_id', Integer, ForeignKey('page.page_id')),
+#                         Column('c_id', Integer, ForeignKey('classes.id'))
+#                         )
 
 
 hypernyms = Table('hypernyms', Base.metadata,
@@ -58,16 +63,11 @@ hypernyms = Table('hypernyms', Base.metadata,
                   )
 
 
-article_types = Table('article_types', Base.metadata,
-                      Column(
-                          'a_id', Integer, ForeignKey('page.page_id')),
-                      Column('t_id', Integer, ForeignKey('types.id'))
-                      )
+# article_types = Table('article_types', Base.metadata,
+#                       Column(
+#                           'a_id', Integer, ForeignKey('page.page_id')),
+#                       Column('t_id', Integer, ForeignKey('types.id'))
+#                       )
 
 
-if __name__ == '__main__':
-    # connect to MySQL and create all tables
-    from sqlalchemy import create_engine
-    engine = create_engine(
-        'mysql+mysqldb://root:@localhost/py_wikipedia', pool_recycle=3600)
-    Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
