38bff716b3b3c84e4f59670d9c49534f51e0ca17
Michael Silver <yomichael88@yahoo.com>
2015-08-23 19:36:44 -0400

    untested get_all_article_classes using WikiExtractor files

--- wikithingsdb/create.py
+++ wikithingsdb/create.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import os
 import argparse
 from wikimap import data
 from defexpand import infoclass
@@ -8,6 +9,8 @@ from wikithingsdb import models
 from sqlalchemy.orm import sessionmaker
 
 Session = sessionmaker(bind=engine)
+title_infobox_re = re.compile(
+    "<doc.*?title\s*?=\s*?\"(.*?)\"\s*?infobox\s*?=\s*?\"(.*?)\".*?>")
 
 
 # DB-INTERFACING
@@ -42,9 +45,17 @@ def insert_article_types(article, a_types):
 
 
 # DATA RETRIEVAL
-def get_all_article_classes():
-    # for each article, get inofoboxes
-    # insert_article_wiki_classes(article, infoboxes)
+def get_all_article_classes(extracted_dir):
+    for root, dirs, files in os.walk(extracted_dir):
+        for file in files:
+            with open(os.path.join(dirpath, file)) as f:
+                for line in f:
+                    result = title_infobox_re.match(line)
+                    if result:
+                        article = result.group(1)
+                        _boxes_str = result.group(2)
+                        infoboxes = [x.strip() for x in _boxes_str.split(",")]
+                        insert_article_wiki_classes(article, infoboxes)
 
 
 def get_all_class_hypernyms(infoboxes):
