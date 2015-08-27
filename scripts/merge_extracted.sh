e4fb98cd689bb3a0dc851bba2be0904cd1630ea0
Michael Silver <yomichael88@yahoo.com>
2015-08-27 14:15:35 -0400

    cleanup & and <br> in xml

--- scripts/merge_extracted.sh
+++ scripts/merge_extracted.sh
@@ -4,6 +4,7 @@
 # one merged.xml file
 
 if [ -d "$1" ] && [ -d "$2" ]; then
+    date
     echo "Merging all files under $1 into $2merged.xml"
     find $1 -name wiki_* -print0 | xargs -0 -I file cat file > $2/merged-tmp.xml
 
@@ -15,7 +16,18 @@ if [ -d "$1" ] && [ -d "$2" ]; then
 
     echo "Inserting </root> end of file"
     echo "</root>" >> $2/merged.xml
+
+    echo "Replacing & with &amp;"
+    sed -i 's/&/&amp;/g' $2/merged.xml
+
+    echo "Deleting all <br> tags"
+    sed -i 's/<br>//g' $2/merged.xml
+
+    echo "Deleting all </br> tags"
+    sed -i 's#</br>##g' $2/merged.xml
+
     echo "DONE."
+    date
 else
     echo "Usage: ./merge_extracted.sh /path/to/extracted/ /path/to/output/dir"
 fi
