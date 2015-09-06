5fa4c4a759f5c9e27f1a46748fff1b42c3c16bad
Michael Silver <yomichael88@yahoo.com>
2015-09-06 12:33:52 -0400

    all join statements for fetching other than redirects

---
+++ wikithingsdb/fetch.py
@@ -0,0 +1,73 @@
+from wikithingsdb.engine import engine
+from wikithingsdb.models import Page, WikiClass, Type, DbpediaClass
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+
+session = sessionmaker(bind=engine)()
+
+
+# def _clean_query(query):
+#     query = query.replace(' ', '_')
+#     query = query.lower()
+#     return query
+
+
+# What are all the ways to refer to ARTICLE?
+def types_of_article(article):
+    # article = _clean_query(article)
+    result = session.query(Page).\
+        join(Page.types).\
+        filter(Page.page_title == article).one()
+    return [x.type for x in result.types]
+
+
+def classes_of_article(article):
+    result = session.query(Page).\
+        join(Page.classes).\
+        filter(Page.page_title == article).one()
+    return [x.class_name for x in result.classes]
+
+
+def hypernyms_of_article(article):
+    return {w_class: hypernyms_of_class(w_class)
+            for w_class in classes_of_article(article)}
+
+
+# also
+def hypernyms_of_class(w_class):
+    result = session.query(WikiClass).\
+        join(WikiClass.dbpedia_classes).\
+        filter(WikiClass.class_name == w_class).one()
+    return [x.dpedia_class for x in result.dbpedia_classes]
+
+
+# What are all the articles of TYPE, CLASS, DBPEDIA_CLASS?
+def articles_of_type(given_type):
+    result = session.query(Type).filter_by(type=given_type).one()
+    return [x.page_title for x in result.page]
+
+
+def articles_of_class(w_class):
+    result = session.query(WikiClass).filter_by(class_name=w_class).one()
+    return [x.page_title for x in result.page]
+
+
+def articles_of_hypernym(hypernym):
+    return {w_class: articles_of_class(w_class)
+            for w_class in classes_under_hypernym(hypernym)}
+
+
+# also
+def classes_of_hypernym(hypernym):
+    result = session.query(DbpediaClass).\
+        filter_by(dpedia_class=hypernym).one()
+    return [x.class_name for x in result.classes]
+
+
+# Symbols & Synonyms from redirects
+def redirects_of_article(article):
+    pass
+
+
+def articles_of_redirect(synonym):
+    pass
