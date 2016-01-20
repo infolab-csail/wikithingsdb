3c0bd3e3b947457a7febac093556d29591b4d6c3
Alvaro Morales <alvarom@csail.mit.edu>
2016-01-20 18:49:31 -0500

    Remove KeyError try/except statements
    peeewee does not raise an exception if an article or type is not found; it just
    returns an empty list.

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -16,26 +16,3 @@ class TestStringConversions(unittest.TestCase):
 
     def test_add_camelcase_thing(self):
         self.assertEqual(fetch._add_camelcase("THing"), "owl:Thing")
-
-
-class TestNotFoundErrors(unittest.TestCase):
-
-    def test_types_of_article_error(self):
-        self.assertRaisesRegexp(
-            KeyError, "No such article: junkjunk123_foo",
-            fetch.types_of_article, "junkjunk123 foo")
-
-    def test_classes_of_article_error(self):
-        self.assertRaisesRegexp(
-            KeyError, "No such article: junkjunk123_foo",
-            fetch.classes_of_article, "junkjunk123 foo")
-
-    def test_hypernyms_of_article_error(self):
-        self.assertRaisesRegexp(
-            KeyError, "No such article: junkjunk123_foo",
-            fetch.hypernyms_of_article, "junkjunk123 foo")
-
-    def test_hypernyms_of_class_error(self):
-        self.assertRaisesRegexp(
-            KeyError, "No such class: junkjunk123 foo",
-            fetch.hypernyms_of_class, "junkjunk123 foo")
--- wikithingsdb/fetch.py
+++ wikithingsdb/fetch.py
@@ -1,7 +1,7 @@
 import re
 import sys
 
-from peewee import DoesNotExist, fn
+from peewee import fn
 
 from wikithingsdb.create import get_hypernyms
 from wikithingsdb.models import Article, Type, WikiClass, DbpediaClass,\
@@ -17,18 +17,13 @@ def types_of_article(article, limit=sys.maxint):
     Given a case-sensitive article title, return the types
     extracted from the article's first sentence by Whoami (as a list
     of strings).
-
-    If no such article is found, raises a KeyError.
     """
-    try:
-        result = (Type
-                  .select()
-                  .join(ArticleType)
-                  .join(Article)
-                  .where(Article.title == article)
-                  .limit(limit))
-    except DoesNotExist:
-        raise KeyError("No such article: " + article)
+    result = (Type
+              .select()
+              .join(ArticleType)
+              .join(Article)
+              .where(Article.title == article)
+              .limit(limit))
 
     return [x.type for x in result]
 
@@ -37,19 +32,13 @@ def classes_of_article(article, limit=sys.maxint):
     """
     Given a case-sensitive article title, return the infoboxes of
     the article (as a list of strings).
-
-    If no such article is found, raises a KeyError.
     """
-    try:
-        result = (WikiClass
-                  .select()
-                  .join(ArticleClass)
-                  .join(Article)
-                  .where(Article.title == article)
-                  .limit(limit))
-    except DoesNotExist:
-        raise KeyError("No such article: " + article)
-
+    result = (WikiClass
+              .select()
+              .join(ArticleClass)
+              .join(Article)
+              .where(Article.title == article)
+              .limit(limit))
     return [x.class_name for x in result]
 
 
@@ -58,8 +47,6 @@ def hypernyms_of_article(article):
     Given a case-sensitive article title, return a dictionary where
     each key is an infobox of the article and its values are a list of
     hypernyms (as a list of strings) from DBpedia's Ontology Classes.
-
-    If no such article is found, raises a KeyError.
     """
     return {w_class: hypernyms_of_class(w_class)
             for w_class in classes_of_article(article)}
@@ -70,22 +57,16 @@ def hypernyms_of_article_from_db(article, limit=sys.maxint):
     Given a case-sensitive article title, return all hypernyms of
     that article (as a list of strings) from DBpedia's Ontology
     Classes.
-
-    If no such article is found, raises a KeyError.
     """
-    try:
-        result = (DbpediaClass
-                  .select()
-                  .join(Hypernym)
-                  .join(ArticleClass, on=Hypernym.c_id)
-                  .join(Article, on=ArticleClass.a_id)
-                  .where(
-                      Article.title == article,
-                  )
-                  .limit(limit))
-    except DoesNotExist:
-        raise KeyError("No such article: " + article)
-
+    result = (DbpediaClass
+              .select()
+              .join(Hypernym)
+              .join(ArticleClass, on=Hypernym.c_id)
+              .join(Article, on=ArticleClass.a_id)
+              .where(
+                  Article.title == article,
+              )
+              .limit(limit))
     return [_remove_camelcase(x.dbpedia_class) for x in result]
 
 
@@ -94,16 +75,10 @@ def hypernyms_of_class(w_class):
     Given a lowercase infobox name (string with hyphens instead of
     spaces), return a list of hypernyms (as a list of strings) from
     DBpedia's Ontology Classes.
-
-    If no such class is found, raises a KeyError.
     """
 
     result = get_hypernyms(w_class)
-
-    if result == []:
-        raise KeyError("No such class: " + w_class)
-    else:
-        return [_remove_camelcase(x) for x in result]
+    return [_remove_camelcase(x) for x in result]
 
 
 def hypernyms_of_class_from_db(w_class, limit=sys.maxint):
@@ -112,22 +87,16 @@ def hypernyms_of_class_from_db(w_class, limit=sys.maxint):
     spaces), return a list of hypernyms (as a list of strings) from
     DBpedia's Ontology Classes.
 
-    If no such class is found, raises a KeyError.
-
     Deprecated: uses the database, resulting in slow, unordered
     results. Use hypernyms_of_class() instead.
     """
 
-    try:
-        result = (DbpediaClass
-                  .select()
-                  .join(Hypernym)
-                  .join(WikiClass)
-                  .where(WikiClass.class_name == w_class)
-                  .limit(limit))
-    except DoesNotExist:
-        raise KeyError("No such class: " + w_class)
-
+    result = (DbpediaClass
+              .select()
+              .join(Hypernym)
+              .join(WikiClass)
+              .where(WikiClass.class_name == w_class)
+              .limit(limit))
     return [_remove_camelcase(x.dbpedia_class) for x in result]
 
 
@@ -135,8 +104,6 @@ def articles_of_type(given_type, limit=sys.maxint):
     """
     Given a lowercase type (spaces allowed, string), return all
     articles of that type.
-
-    If no such type is found, raises a KeyError.
     """
     return articles_with_multiple_types(given_type, op='and', limit=limit)
 
@@ -149,9 +116,9 @@ def articles_with_multiple_types(*types, **kwargs):
     Specify op='and' for an AND query that returns articles with ALL types (default)
     Specify op='or' for an OR query that returns articles with AT LEAST one of types
 
-    Specify limit=n kwarg to limit return values to n rows.
+    If an invalid op is passed, raises a ValueError.
 
-    If no such type is found, raises a KeyError.
+    Specify limit=n kwarg to limit return values to n rows.
     """
 
     # In Python 2, we can't specify *args and named kwargs
@@ -159,29 +126,26 @@ def articles_with_multiple_types(*types, **kwargs):
     op = kwargs.pop('op', 'and')
     limit = kwargs.pop('limit', sys.maxint)
 
-    try:
-        if op == 'and':
-            result = (Article
-                      .select()
-                      .join(ArticleType)
-                      .join(Type)
-                      .where(Type.type << types)
-                      .group_by(Article)
-                      .having(fn.COUNT(Article.id) == len(types))
-                      .limit(limit))
-        elif op == 'or':
-            result = (Article
-                      .select()
-                      .join(ArticleType)
-                      .join(Type)
-                      .where(Type.type << types)
-                      .group_by(Article)
-                      .limit(limit))
-        else:
-            raise ValueError(
-                "Illegal op: {}. Valid options are 'and', 'or'.".format(op))
-    except DoesNotExist:
-        raise KeyError("No such types: " + types)
+    if op == 'and':
+        result = (Article
+                  .select()
+                  .join(ArticleType)
+                  .join(Type)
+                  .where(Type.type << types)
+                  .group_by(Article)
+                  .having(fn.COUNT(Article.id) == len(types))
+                  .limit(limit))
+    elif op == 'or':
+        result = (Article
+                  .select()
+                  .join(ArticleType)
+                  .join(Type)
+                  .where(Type.type << types)
+                  .group_by(Article)
+                  .limit(limit))
+    else:
+        raise ValueError(
+            "Illegal op: {}. Valid options are 'and', 'or'.".format(op))
 
     return [x.title for x in result]
 
@@ -190,19 +154,13 @@ def articles_of_class(w_class, limit=sys.maxint):
     """
     Given a lowercase infobox name (string with hyphens instead of
     spaces), return all articles of that type.
-
-    If no such class is found, raises a KeyError.
     """
-    try:
-        result = (Article
-                  .select()
-                  .join(ArticleClass)
-                  .join(WikiClass)
-                  .where(WikiClass.class_name == w_class)
-                  .limit(limit))
-    except DoesNotExist:
-        raise KeyError("No such class: " + w_class)
-
+    result = (Article
+              .select()
+              .join(ArticleClass)
+              .join(WikiClass)
+              .where(WikiClass.class_name == w_class)
+              .limit(limit))
     return [x.title for x in result]
 
 
@@ -212,8 +170,6 @@ def articles_of_hypernym(hypernym):
     where each key is an infobox (string with hyphens instead of
     spaces) of that hypernym and each value is a list of articles of
     that infobox. Note: use 'thing' instead of 'owl:Thing'.
-
-    If no such hypernym is found, raises a KeyError.
     """
     return {w_class: articles_of_class(w_class)
             for w_class in classes_of_hypernym(hypernym)}
@@ -224,21 +180,15 @@ def articles_of_hypernym_from_db(hypernym, limit=sys.maxint):
     Given a hypernym from DBpedia (string), return a list of
     articles of that hypernym. Note: use 'thing' instead of
     'owl:Thing'.
-
-    If no such hypernym is found, raises a KeyError.
     """
     hypernym = _add_camelcase(hypernym)
-    try:
-        result = (Article
-                  .select()
-                  .join(ArticleClass)
-                  .join(Hypernym, on=ArticleClass.c_id)
-                  .join(DbpediaClass, on=Hypernym.d_id)
-                  .where(DbpediaClass.dbpedia_class == hypernym)
-                  .limit(limit))
-    except DoesNotExist:
-        raise KeyError("No such hypernym: " + hypernym)
-
+    result = (Article
+              .select()
+              .join(ArticleClass)
+              .join(Hypernym, on=ArticleClass.c_id)
+              .join(DbpediaClass, on=Hypernym.d_id)
+              .where(DbpediaClass.dbpedia_class == hypernym)
+              .limit(limit))
     return [x.title for x in result]
 
 
@@ -247,20 +197,14 @@ def classes_of_hypernym(hypernym, limit=sys.maxint):
     Given a hypernym from DBpedia (string), return a list of
     infoboxes of that hypernym (list of strings with hyphens instead
     of spaces). Note: use 'thing' instead of 'owl:Thing'.
-
-    If no such hypernym is found, raises a KeyError.
     """
     hypernym = _add_camelcase(hypernym)
-    try:
-        result = (WikiClass
-                  .select()
-                  .join(Hypernym)
-                  .join(DbpediaClass)
-                  .where(DbpediaClass.dbpedia_class == hypernym)
-                  .limit(limit))
-    except DoesNotExist:
-        raise KeyError("No such hypernym: " + hypernym)
-
+    result = (WikiClass
+              .select()
+              .join(Hypernym)
+              .join(DbpediaClass)
+              .where(DbpediaClass.dbpedia_class == hypernym)
+              .limit(limit))
     return [x.class_name for x in result]
 
 # -------
